58b9e9a47272e2fdcf728057f6e5207299e1210f
Artem Danilov <artemdanilow95@gmail.com>
2020-09-16 12:33:32 +0200

    Make LP responsive for devices with medium screen

--- js/navbar.js
+++ js/navbar.js
@@ -2,5 +2,5 @@ const nav = document.querySelector('.nav');
 
 export function bgNav() {
     const scrollDown = window.pageYOffset;
-    scrollDown >= 100 ? nav.classList.add("scroll-bg") : nav.classList.remove("scroll-bg");
+    scrollDown >= 50 ? nav.classList.add("scroll-bg") : nav.classList.remove("scroll-bg");
 }
