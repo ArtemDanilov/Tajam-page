c5f41730c52b1b6d4f935af1837ce8e23c68e9e9
Artem Danilov <artemdanilow95@gmail.com>
2020-09-20 15:21:01 +0200

    Fix issue with img displaying in team section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ import { bgNav, openNav } from "./navbar.js";
 const btn = document.getElementById("hamburger")
 
 window.addEventListener('scroll', bgNav);
-btn.addEventListener('click', openNav)
+btn.addEventListener('click', openNav);
