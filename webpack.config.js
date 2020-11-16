093791b8e0932c95809ba57b3950a596705a1de4
Artem Danilov <artemdanilow95@gmail.com>
2020-11-16 16:38:06 +0100

    delete webpack config and rename prod -> config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
-    mode: "development",
     entry: {
         main: "./js/main.js"
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "main.bundle.js",
-        publicPath: "dist"
     },
     module: {
         rules: [{
@@ -22,21 +24,61 @@ module.exports = {
                 }
             },
             {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                ]
+            },
+            {
                 test: /\.s(a|c)ss$/,
-                loaders: [
-                    'style-loader',
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'sass-loader'
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
                 ]
             },
             {
                 test: /\.(woff|woff2|ttf|eot|jpe?g|svg|png)$/i,
                 loader: 'file-loader',
                 options: {
-                    name: '[path][name].[ext]',
-                    publicPath: "../",
-                },
+                    name: '[path][name].[ext]'
+                }
             },
         ]
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+        })
+    ],
 }
