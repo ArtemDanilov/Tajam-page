2c4102cbbfc9b311d9b196f9fd508e4379798ac6
Artem Danilov <artemdanilow95@gmail.com>
2020-11-16 13:19:55 +0100

    Webpack development configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,42 @@
 const path = require("path");
 
 module.exports = {
+    mode: "development",
     entry: {
-       main: "./js/main.js"
+        main: "./js/main.js"
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "main.bundle.js"
+        filename: "main.bundle.js",
+        publicPath: "dist"
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
-                      }
+                    }
                 }
-            }
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot|jpe?g|svg|png)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                    publicPath: "../",
+                },
+            },
         ]
-    }
+    },
 }
