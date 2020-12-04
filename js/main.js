9888d5b09a648e27ccb9f435b6be0d76f244e06d
Artem Danilov <artemdanilow95@gmail.com>
2020-12-04 14:05:23 +0100

    Create a slider in people section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import "../sass/main.scss"
+import "./headerSlider"
 
 import { bgNav, openNav, smoothScroll } from "./navbar.js";
-import "./slider.js";
+import peopleSlider from "./peopleSlider"
 
 const btn = document.getElementById("hamburger")
 const links = document.querySelectorAll(".nav__list--item");
@@ -12,3 +13,4 @@ for (const link of links) {
 
 window.addEventListener('scroll', bgNav);
 btn.addEventListener('click', openNav);
+peopleSlider()
---
+++ js/peopleSlider.js
@@ -0,0 +1,60 @@
+const people = [...document.querySelectorAll(".people__img-container--img")];
+
+const prevBtn = document.getElementById("prevBtn")
+const nextBtn = document.getElementById("nextBtn")
+
+const fullName = document.getElementById("targetName")
+const profession = document.getElementById("targetProfession")
+
+const peopleData = [
+    {
+        fullName: "Mark Ford",
+        profession: "sale specialist"
+    },
+    {
+        fullName: "Geneva Yakovlevna",
+        profession: "project manager"
+    },
+    {
+        fullName: "Joe Hook",
+        profession: "graphic designer"
+    }
+]
+
+const div = document.createElement('div')
+let index = 1;
+
+export default function personPresentation() {
+    div.innerHTML = `
+    <h4 class="heading-4 u-margin-top--m people__name" id="targetName">${peopleData[index].fullName}</h4>
+    <p class="paragraph people__profession" id="targetProfession">${peopleData[index].profession}</p> `
+    
+    document.querySelector('.people__slider').before(div)
+    div.classList.add('people__present')
+}
+
+prevBtn.addEventListener('click', () => {
+    people[index].classList.remove("img-active")
+    
+    if (index === 0) index = people.length;
+    index--
+    
+    people[index].classList.add("img-active") 
+    
+    personPresentation()
+    fullName.innerHTML = peopleData[index].fullName
+    profession.innerHTML = peopleData[index].profession
+})
+
+nextBtn.addEventListener('click', () => {
+    people[index].classList.remove("img-active")
+    
+    if (index === people.length - 1) index = -1;
+    index++
+    
+    people[index].classList.add("img-active")
+    
+    personPresentation()
+    fullName.innerHTML = peopleData[index].fullName
+    profession.innerHTML = peopleData[index].profession
+})
